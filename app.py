149201ae3bdbdbb4c58d7918dc0daf716869b457
Azhar <100113915+AzharSheikh1@users.noreply.github.com>
2022-08-23 16:42:19 +0530

    conversion of a file from pbl to csv

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('testdata.csv')
-df = df.filter(['Unique Sector ID', 'Raw Position', 'Improved Position', 'Number Improved Position Entries'])
+#simple file conversion (from pbl to csv)
+filename = 'inputdata.pbl'
+with open(filename, 'r') as f, open('csvfile.csv', 'w') as outfile:
+    for x in f.readlines():
+        if not x.strip().startswith('%') and x.strip():
+            outfile.write(x.strip() + '\n')
+
+df = pd.read_csv('csvfile.csv')
+df = df.filter(['Unique Sector ID', ' Raw Position', ' Improved Position', ' Number Improved Position Entries'])
 
 df['latitude'],df['longitude'] = np.where(
-                                    df['Number Improved Position Entries'] >= 1, 
+                                    df[' Number Improved Position Entries'] >= 1, 
                                     (   
-                                        df['Improved Position'].str.split(';').str[0].str.slice(2), 
-                                        df['Improved Position'].str.split(';').str[1].str.slice(2)
+                                        df[' Improved Position'].str.split(';').str[0].str.slice(2), 
+                                        df[' Improved Position'].str.split(';').str[1].str.slice(2)
                                     ), 
                                     (   
-                                        df['Raw Position'].str.split(';').str[0].str.slice(2), 
-                                        df['Raw Position'].str.split(';').str[1].str.slice(2)
+                                        df[' Raw Position'].str.split(';').str[0].str.slice(2), 
+                                        df[' Raw Position'].str.split(';').str[1].str.slice(2)
                                     ))
 
-df = df.drop(columns=['Raw Position', 'Improved Position', 'Number Improved Position Entries'])
+df = df.drop(columns=[' Raw Position', ' Improved Position', ' Number Improved Position Entries'])
 
 df.to_csv('result.csv', encoding='utf-8')
