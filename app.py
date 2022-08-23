85354a952cd2a371f3325a84656e1c7367213474
Azhar <100113915+AzharSheikh1@users.noreply.github.com>
2022-08-23 14:21:15 +0530

    remove repeatation

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ import numpy as np
 df = pd.read_csv('testdata.csv')
 df = df.filter(['Unique Sector ID', 'Raw Position', 'Improved Position', 'Number Improved Position Entries'])
 
-x = df.loc[0:df.shape[0], ['Unique Sector ID','Raw Position', 'Number Improved Position Entries', 'Improved Position']]
-df['latitude'] = np.where(df['Number Improved Position Entries'] >= 1, df['Improved Position'].str.split(';').str[0].str.slice(2), df['Raw Position'].str.split(';').str[0].str.slice(2))
-df['longitude'] = np.where(df['Number Improved Position Entries'] >= 1, df['Improved Position'].str.split(';').str[1].str.slice(2), df['Raw Position'].str.split(';').str[1].str.slice(2))
+df['latitude'],df['longitude'] = np.where(
+                                    df['Number Improved Position Entries'] >= 1, 
+                                    (   
+                                        df['Improved Position'].str.split(';').str[0].str.slice(2), 
+                                        df['Improved Position'].str.split(';').str[1].str.slice(2)
+                                    ), 
+                                    (   
+                                        df['Raw Position'].str.split(';').str[0].str.slice(2), 
+                                        df['Raw Position'].str.split(';').str[1].str.slice(2)
+                                    ))
 
 df = df.drop(columns=['Raw Position', 'Improved Position', 'Number Improved Position Entries'])
 
